c75f0150f40157e473e0c25b8546b41ee36de935
Zach Marbach <Zach.Marbach@improving.com>
2020-01-10 10:33:38 -0600

    added mapping to make owners index page show a list of owners

--- pet-clinic-web/src/main/java/com/improving/sfgpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/improving/sfgpetclinic/controllers/OwnerController.java
@@ -1,13 +1,22 @@
 package com.improving.sfgpetclinic.controllers;
 
+import com.improving.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController( OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping("/owners")
-    public String ownerIndex(){
+    public String ownerIndex(Model model){
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
