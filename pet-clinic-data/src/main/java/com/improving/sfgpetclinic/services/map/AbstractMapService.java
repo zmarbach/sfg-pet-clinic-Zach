21ffe91be89d55301142cb32915060fcf0d4a4db
Zach Marbach <Zach.Marbach@improving.com>
2020-01-10 11:06:44 -0600

    refactored service layer to auto-generate ID for entities to mimic what Spring JPA will do.

--- pet-clinic-data/src/main/java/com/improving/sfgpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/improving/sfgpetclinic/services/map/AbstractMapService.java
@@ -1,12 +1,11 @@
 package com.improving.sfgpetclinic.services.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import com.improving.sfgpetclinic.models.BaseEntity;
 
-public abstract class AbstractMapService<T, ID> {
-    protected Map<ID, T> map = new HashMap<>();
+import java.util.*;
+
+public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
+    protected Map<Long, T> map = new HashMap<>();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -16,8 +15,16 @@ public abstract class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T obj){
-        map.put(id, obj);
+    T save(T obj){
+        if(obj != null){
+            if(obj.getId() == null) {
+                obj.setId(getNextId());
+            }
+            map.put(obj.getId(), obj);
+        }
+        else{
+            throw new RuntimeException("Object cannot be null");
+        }
         return obj;
     }
 
@@ -28,4 +35,16 @@ public abstract class AbstractMapService<T, ID> {
     void delete(T obj) {
         map.entrySet().removeIf(item -> item.getValue().equals(obj));
     }
+
+    private Long getNextId() {
+        Long nextId = null;
+            try{
+                nextId = Collections.max(map.keySet()) + 1;
+            }
+            catch(NoSuchElementException e){
+                nextId = 1L;
+            }
+        return nextId;
+    }
 }
+
--- pet-clinic-web/src/main/java/com/improving/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/improving/sfgpetclinic/bootstrap/DataLoader.java
@@ -25,14 +25,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Ross");
         owner1.setLastName("Geller");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Rachel");
         owner2.setLastName("Green");
 
@@ -41,14 +39,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Chandler");
         vet1.setLastName("Bing");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Joey");
         vet2.setLastName("Tribiani");
 
