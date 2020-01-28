6ecf82e10d0372b526d113b48e72629b8f636566
Zach Marbach <zachary.marbach@improving.com>
2020-01-28 10:12:13 -0600

    added vet JSON endpoint

--- pet-clinic-web/src/main/java/com/improving/sfgpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/improving/sfgpetclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.improving.sfgpetclinic.controllers;
 
+import com.improving.sfgpetclinic.models.Vet;
 import com.improving.sfgpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 @Controller
 public class VetController {
@@ -14,9 +19,15 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets.html"})
+    @GetMapping({"/vets", "/vets.html"})
     public String vetIndex(Model model){
         model.addAttribute("vets", vetService.findAll());
         return "vets/index";
+    }
+
+    @GetMapping("/api/vets")
+    //@ResponseBody tells Spring to bind whatever we return out of the method to the HTTP Response Body (either JSON or XML)
+    public @ResponseBody Set<Vet> getVetsJson(){
+        return vetService.findAll();
     }
 }
